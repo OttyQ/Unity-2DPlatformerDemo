4a31ea175c4124127eeabe25882882e0b1e3e3e3
OttyQ <eugene.hatskevich25@mail.ru>
2024-09-09 22:16:29 +0300

    Base

---
+++ Assets/Scripts/Helper.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Helper
+{
+    public static float Map(float value, float originalMin, float originalMax, float newMin, float newMax, bool clamp) {
+        float newValue = (value - originalMin) / (originalMax - originalMin) * (newMax - newMin) + newMin; 
+        if (clamp) {
+            newValue = Mathf.Clamp(newValue, newMin, newMax);
+        }
+    return newValue; 
+    }
+}
